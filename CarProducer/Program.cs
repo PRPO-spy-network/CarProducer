f5f45761b01674f531a05d1e2ae966d4dd1590e8
NahtigalTim <sirislyyofriend@gmail.com>
2025-11-26 19:01:05 +0100

    Wops

--- CarProducer/Program.cs
+++ CarProducer/Program.cs
@@ -46,7 +46,8 @@ builder.Services.AddHttpClient<IRegions, RegionsApi>()
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); 
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
